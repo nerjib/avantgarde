338d488d37ff223bdfe754fc02ae9ecd9bcfee4c
nerjib <kabirnajib0@gmail.com>
2022-09-03 12:48:28 +0100

    Updates

---
+++ precache-manifest.552ce6461304fe9b579db131d0280022.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e748044938ca13d496f12afe81a772c",
+    "url": "/avantgarde/index.html"
+  },
+  {
+    "revision": "c8fac73dd5854e4ec61b",
+    "url": "/avantgarde/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "c0f89d971dcdedbf380b",
+    "url": "/avantgarde/static/css/main.e3f071c8.chunk.css"
+  },
+  {
+    "revision": "c8fac73dd5854e4ec61b",
+    "url": "/avantgarde/static/js/2.a04b1633.chunk.js"
+  },
+  {
+    "revision": "3f2ad72d1d7871920b9567ec2e765bb9",
+    "url": "/avantgarde/static/js/2.a04b1633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f89d971dcdedbf380b",
+    "url": "/avantgarde/static/js/main.2d365f85.chunk.js"
+  },
+  {
+    "revision": "8e0c68c5bf3de8dd8733",
+    "url": "/avantgarde/static/js/runtime-main.04b1258a.js"
+  },
+  {
+    "revision": "636c3af61771f8f62f703413ec253a68",
+    "url": "/avantgarde/static/media/kaduna-map.636c3af6.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/avantgarde/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "02437f3f4165922e6c7e9af6b239a007",
+    "url": "/avantgarde/static/media/ruwasa.02437f3f.jpg"
+  },
+  {
+    "revision": "2b0f3c4d5792684eddbbd1e10638dfb1",
+    "url": "/avantgarde/static/media/ruwasa.2b0f3c4d.png"
+  },
+  {
+    "revision": "59d726b5100eba9e183590ea2385b97a",
+    "url": "/avantgarde/static/media/ruwasa.59d726b5.jpg"
+  },
+  {
+    "revision": "41fd881bc2d54d72e92737e34ef0037d",
+    "url": "/avantgarde/static/media/ruwassa.41fd881b.PNG"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/avantgarde/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "c93fc9232362e91fcb847db2ee7e7bcd",
+    "url": "/avantgarde/static/media/sidebar-2.c93fc923.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/avantgarde/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/avantgarde/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "ed6e184d125128ea2b6b7ac81ccc5e01",
+    "url": "/avantgarde/static/media/ukaidd.ed6e184d.jpg"
+  },
+  {
+    "revision": "d9796521f21d6d4e1b39e1a6ba8848c6",
+    "url": "/avantgarde/static/media/unicef.d9796521.png"
+  }
+]);
